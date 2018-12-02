4321f767beae6b4d5290e29167cbc4cd25d43d27
13shi <wirepuller13@gmail.com>
2018-12-02 16:40:07 +0900

    カード追加

--- star2/index.js
+++ star2/index.js
@@ -1,18 +1,6 @@
-$(function() {
- 
-  $(".modal").fancybox({
-    'onComplete': function() {
-      vidplay();
-    }
-  });
- 
-  function vidplay() {
-    var video = document.getElementById("player");
-    if (video.paused) {
-      video.play();
-    } else {
-      video.pause();
-    }
+/* Demo purposes only */
+$(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
   }
- 
-});
+);
