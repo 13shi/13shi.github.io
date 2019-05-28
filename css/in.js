b16c5b95c646405964ab423e6d8bbd5607b2b3eb
13shi <38181007+13shi@users.noreply.github.com>
2019-05-28 11:31:11 +0900

    ポートフォリオサイト

---
+++ css/in.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+  var zindex = 10;
+  
+  $("div.card").click(function(e){
+    e.preventDefault();
+
+    var isShowing = false;
+
+    if ($(this).hasClass("show")) {
+      isShowing = true
+    }
+
+    if ($("div.cards").hasClass("showing")) {
+      // a card is already in view
+      $("div.card.show")
+        .removeClass("show");
+
+      if (isShowing) {
+        // this card was showing - reset the grid
+        $("div.cards")
+          .removeClass("showing");
+      } else {
+        // this card isn't showing - get in with it
+        $(this)
+          .css({zIndex: zindex})
+          .addClass("show");
+
+      }
+
+      zindex++;
+
+    } else {
+      // no cards in view
+      $("div.cards")
+        .addClass("showing");
+      $(this)
+        .css({zIndex:zindex})
+        .addClass("show");
+
+      zindex++;
+    }
+    
+  });
+});
+
+
+
+
+/**
+* 折り畳み表示
+* 'openHere'というクラスに属するオブジェクトをインライン要素or非表示に変更する。
+*/
+function openClose(){
+    var obj = document.getElementsByClassName('openHere');
+    for(var i=0;i<obj.length;i++){
+        //非表示ならインライン要素に変更。表示状態なら非表示に変更。
+        if(obj[i].style.display == "inline-block"){
+            obj[i].style.display = "none";
+        }
+        else{
+            obj[i].style.display = "inline-block";
+        }
+    }
+}
